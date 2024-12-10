622ab87a5e85ab98a98c9d1b369a63231e04432c
ruben asua <rubenasuasoto@gmail.com>
2024-12-10 21:28:03 +0100

    Initial commit

---
+++ hilos_game/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static Mutex mutex = new Mutex();
+
+    static void Main()
+    {
+        Thread thread1 = new Thread(MethodUsingMutex);
+        Thread thread2 = new Thread(MethodUsingMutex);
+        
+        thread1.Start();
+        thread2.Start();
+        for (int i = 0; i < 1000; i++) Console.Write ("x"); 
+    }
+
+    static void MethodUsingMutex()
+    {
+        int r;
+        
+        mutex.WaitOne();
+
+        Random rnd = new Random();
+        r = rnd.Next(1, 4);
+        
+
+        mutex.ReleaseMutex();
+        
+        
+        
+    }
+}
